b147b6d105e816853b665e1992ff303067a08b6b
uitop500 <qwr107181@gmail.com>
2025-07-14 18:55:01 +0800

    Update DeviceModelChecker.kt
    checkIsNotUFI

--- app/src/main/java/com/minikano/f50_sms/utils/DeviceModelChecker.kt
+++ app/src/main/java/com/minikano/f50_sms/utils/DeviceModelChecker.kt
@@ -3,6 +3,7 @@ package com.minikano.f50_sms.utils
 import android.content.Context
 import android.os.Build
 import android.util.Log
+import android.widget.Toast
 
 object DeviceModelChecker {
     private var isUnSupportDevice = false
@@ -19,29 +20,12 @@ object DeviceModelChecker {
     )
 
     fun checkBlackList(): Boolean {
-        Log.d("kano_ZTE_LOG_devcheck", "正在遍历黑名单设备...")
-        val model = Build.MODEL.trim()
-        val firmwareVersion = Build.DISPLAY
-        devicesBlackList.forEach {
-            Log.d("kano_ZTE_LOG_devcheck", "$it == $model ?")
-            if (it.trim().contains(model)) {
-                isUnSupportDevice = true
-            }
-        }
-        frimwareWhiteList.forEach{
-            if (firmwareVersion.contains(it.trim())) {
-                Log.d("kano_ZTE_LOG_devcheck", "检测到白名单固件，放行")
-                
-            }
-        }
-        isUnSupportDevice = false
-        return isUnSupportDevice
+        Toast.makeText(applicationContext, "checkBlackList", Toast.LENGTH_LONG).show()
+        return false
     }
 
     fun checkIsNotUFI(context: Context):Boolean{
-        val isUFI_0 = KanoUtils.isAppInstalled(context,"com.zte.web")
-        val isUFI = ShellKano.runShellCommand("pm list package")
-        Log.d("kano_ZTE_LOG_devcheck", "isUFI_0：${isUFI_0},has com.zte.web? :${isUFI?.contains("com.zte.web")} ")
+        Toast.makeText(applicationContext, "checkIsNotUFI", Toast.LENGTH_LONG).show()
         return false
     }
 }
